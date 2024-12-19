80ca591f2f8aa0195f115d2e1b0dcb5f5711b357
namanxgarg <social.namangarg@gmail.com>
2024-12-19 14:27:28 +0530

    created a basic runner

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting self-hosted GitHub Actions...")
+
+	// Simulate a runner
+	for {
+		fmt.Println("Polling for jobs...")
+		time.Sleep(5 * time.Second)
+	}
 }
